ec399f19326b13180f039d1bc682a0ee7cd63af5
Denis Myachin <denchikgspk@gmail.com>
2021-10-10 19:35:36 +0700

    add fonts

--- sw_messenger/App.js
+++ sw_messenger/App.js
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
+import Icon from './components/Icon';
 import {
   SafeAreaView,
   ScrollView,
@@ -18,95 +10,51 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+export const App = () => {
+  
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <SafeAreaView style={{flex: 1}}>
+      {/* <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} /> */}
+      <Text style={{fontFamily: 'HKGrotesk-Bold', fontSize: 16}}>Hello world</Text>
+      <Text style={{fontFamily: 'HKGrotesk-Medium', fontSize: 16}}>Hello world</Text>
+      <Text style={{fontFamily: 'HKGrotesk-Regular', fontSize: 16}}>Hello world</Text>
+      <Text style={{fontFamily: 'HKGrotesk-SemiBold', fontSize: 16}}>Hello world</Text>
+      
+      <View style={styles.body}>
+        <Icon name="share" size={24} color="blue" />
+        <Icon name="settings-cog" size={24} color="blue" />
+        <Icon name="search" size={24} color="blue" />
+        <Icon name="paper-airplane" size={24} color="blue" />
+        <Icon name="menu" size={24} color="blue" />
+        <Icon name="logout" size={24} color="blue" />
+        <Icon name="location-marker" size={24} color="blue" />
+        <Icon name="information-circle" size={24} color="blue" />
+        <Icon name="group" size={24} color="blue" />
+        <Icon name="document-duplicate" size={24} color="blue" />
+        <Icon name="color-swatch" size={24} color="blue" />
+        <Icon name="chevron-up" size={24} color="blue" />
+        <Icon name="cake" size={24} color="blue" />
+        <Icon name="briefcase" size={24} color="blue" />
+        <Icon name="backspace" size={24} color="blue" />
+        <Icon name="Profile" size={24} color="blue" />
+        <Icon name="Like" size={24} color="blue" />
+        <Icon name="Dots_Vertical" size={24} color="blue" />
+        <Icon name="Bookmark" size={24} color="blue" />
+        <Icon name="Arrow" size={24} color="blue" />
+        <Icon name="Arrow_Left" size={24} color="blue" />
+        <Icon name="Alert" size={24} color="blue" />
+        <Icon name="instagram" size={24} color="blue" />
+        <Icon name="twitter" size={24} color="blue" />
+        <Icon name="telegram" size={24} color="blue" />
+        <Icon name="facebook" size={24} color="blue" />
+        <Icon name="chat" size={24} color="blue" />
     </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  
 });
 
 export default App;
